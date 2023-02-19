4fa06f865710298dacde8ab3c6a4a13b5ea9be27
Ata Parlar <ataparlar@leodrive.ai>
2023-02-19 14:40:34 +0300

    made only one exec str

--- cloud_editer.py
+++ cloud_editer.py
@@ -1,5 +1,7 @@
-import os
+#!/home/ataparlar/miniconda3/envs/CloudComPy310/bin/python
 
+import os
+from keyboard import press
 # CloudCompare
 # -O 2021-09-18-10-19-24_Velodyne-HDL-32-Data.las
 # -SS OCTREE 16
@@ -11,8 +13,8 @@ import os
 # -FEATURE LINEARITY 0.1
 # -SAVE_CLOUDS ALL_AT_ONCE FILE test.bin
 
-directory = "/home/ataparlar/data/hdmap_paper_data/test_clouds/"
-terminal_exec_str = "CloudCompare -O "
+directory = "/home/ataparlar/data/hd_map_paper_data/PROCESS/LAS/uploaded/"
+terminal_exec_str = "CloudCompare"
 file_counter = 0
 file_list = []
 
@@ -22,20 +24,24 @@ for filename in os.listdir(directory):
     file_list.append(filepath)
     print("files collected")
 
+file_list.sort()
+
 for filepath in file_list:
     print("file '" + filepath + "' is calculating")
 
-    new_file_name = "{:06d}".format(file_counter) + ".bin"
-    file_counter += 1
+    # new_file_name = "{:06d}".format(file_counter) + ".bin"
+    # file_counter += 1
 
-    terminal_exec_str += filepath
-    print("execution")
-    terminal_exec_str += " -SS OCTREE 16 -SOR 6 1 -NOISE KNN 6 REL 1.0 RIP -OCTREE_NORMALS auto -MODEL QUADRIC -OCTREE_NORMALS auto -MODEL LS -FEATURE PLANARITY 0.1 -FEATURE LINEARITY 0.1 -SAVE_CLOUDS ALL_AT_ONCE FILE "
-    terminal_exec_str += new_file_name
-    os.system(terminal_exec_str)
-    print("executed")
+    terminal_exec_str += (" -O " + filepath)
+    # terminal_exec_str += new_file_name
+    # press('enter')
 
-    terminal_exec_str = "CloudCompare -O "
+
+terminal_exec_str += " -SS OCTREE 16 -SOR 6 1 -NOISE KNN 6 REL 1.0 RIP -OCTREE_NORMALS auto -MODEL QUADRIC -OCTREE_NORMALS auto -MODEL LS -FEATURE PLANARITY 0.1 -FEATURE LINEARITY 0.1 -MERGE_CLOUDS -SAVE_CLOUDS ALL_AT_ONCE FILE merged_cloud"
+print("execution of \n\t" + terminal_exec_str)
+os.system(terminal_exec_str)
+print("executed")
+
 
 print("done -------------------")
 
---
+++ press_enter.py
@@ -0,0 +1,9 @@
+#!/home/ataparlar/miniconda3/envs/CloudComPy310/bin/python
+
+import time
+from keyboard import press
+
+while True:
+    press('enter')
+    print("pressed enter")
+    time.sleep(300)
